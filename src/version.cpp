b4a606ef6cd6d1d4afa8756ff11abca3a72f049f
Gordon Pitcher <gorgorom90@gmail.com>
2013-12-06 20:38:37 -0330

    1.0.0.1 changes
    new logo, gui changes, minor fixes to code.

--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Paycoin developers
+// Copyright (c) 2012 The Paycoin developers, The Novacoin Developers, The Litecoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <string>
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("PayCoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-pyc"
+#define CLIENT_VERSION_SUFFIX   "-PAY"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
